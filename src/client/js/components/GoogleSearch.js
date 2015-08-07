e702b597d3f7592c4e2ab7d73588a3bfac177afd
WillMayger <william.mayger@atos.net>
2015-08-07 16:23:38 +0100

    adding if statement

--- src/client/js/components/GoogleSearch.js
+++ src/client/js/components/GoogleSearch.js
@@ -2,22 +2,24 @@
 
 var React = require('../../../../node_modules/react/react.js');
 // react component DateBox will return a date time that updates every 50 miliseconds and appends it to a div with an id of 'date_box'
-
-function GetValueFromGoogleSearchInput(){
-        var GoogleSearch = document.getElementById("google_search_input");
-        var UserSearchQuery = GoogleSearch.value.toString().replace(/ /g,"+");
-        var GET = "http://google.com/#q=" + UserSearchQuery;
-        window.open(GET, '_blank');
-    };
 var GoogleSearchBox = React.createClass({
+    GetValueFromGoogleSearchInput: function(event){
+    	var GoogleSearch = document.getElementById("google_search_input");
+        if (GoogleSearch.value.toString() != ""){
+
+
+    	var UserSearchQuery = GoogleSearch.value.toString().replace(/ /g,"+");
+    	var GET = "http://google.com/#q=" + UserSearchQuery;
+    	window.open(GET, '_blank');
+    };
+    },
     //rendering the html elements with the state.clock
     render: function() {
         return (
             <div className="google_search">
-            <input type="hidden"></input>
                 <input type="text" value="" placeholder="Type here to search google" name="google_search_input" id="google_search_input">
                 </input>
-                <button id="google_search_button" onClick={GetValueFromGoogleSearchInput()}></button>
+                <button id="google_search_button" onClick={this.GetValueFromGoogleSearchInput}></button>
             </div>
         );
     }
