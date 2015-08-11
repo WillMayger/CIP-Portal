e06af593fcba53e90865fc4708033995c478dea0
WillMayger <william.mayger@atos.net>
2015-08-11 12:34:14 +0100

    send glyphicon

--- src/client/js/components/ShoutBox.js
+++ src/client/js/components/ShoutBox.js
@@ -38,7 +38,7 @@ var ShoutBoxInput = React.createClass({
     return (
       <div className="shout_box_input">
       <input type="text" className="form-control" id="shout_box_input" value={this.state.value} onChange={this.handleChange}>
-      </input> <span class="glyphicon glyphicon-menu-right" aria-hidden="true" onClick={this.SendInfo} ></span>
+      </input> <span onClick={this.SendInfo} className="input-group-btn"><span className="glyphicon glyphicon-menu-right" aria-hidden="true"></span></span>
       </div>
     );
   }  
